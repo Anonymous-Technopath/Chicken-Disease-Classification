fc4dd583a595c23eb18cab4fb31c12de0a307b91
Nimish Shandilya <technopath2003@gmail.com>
2024-05-13 11:09:34 +0530

    minor optimization

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -1,6 +1,3 @@
-import os
-import urllib.request as request
-from zipfile import ZipFile
 import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
